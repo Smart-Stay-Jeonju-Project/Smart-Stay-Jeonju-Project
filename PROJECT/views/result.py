432ec086dc1b997d5a6396fe98b8046cdb3d2ee9
2taeyang <lsun058@naver.com>
2025-06-27 14:09:33 +0900

    ㄱㄴㄷㄹ

---
+++ PROJECT/views/result.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template
+
+bp = Blueprint('result', __name__, url_prefix='/result')
+
+@bp.route('/')
+def result() : 
+    print("result.py :: result()")
+    # 상세 페이지
+    return render_template('result.html')
+
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
+# 이 페이지 삭제 고민
---
+++ PROJECT/views/search.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, request
+
+bp = Blueprint('search', __name__, url_prefix='/search')
+
+@bp.route('/', methods=['GET'])
+def search() : 
+    print("search.py :: search()")
+    type = request.args.get('search_type')
+    word = request.args.get('keyword')
+    print(f"search_type {type}")
+    print(f"keyword {word}")
+    # 검색결과 페이지
+    return render_template('search.html')
