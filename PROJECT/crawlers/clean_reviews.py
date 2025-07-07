786ee8bac6f51fa8db30760c1ab9255c2acd62ea
jhwo9412 <jhwo9412@gmail.com>
2025-07-07 17:00:37 +0900

    각 숙소별 리뷰 정제 코드 추가

--- PROJECT/crawlers/clean_reviews.py
+++ PROJECT/crawlers/clean_reviews.py
@@ -1,18 +1,14 @@
 import os
 import pandas as pd
-import re
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 
-targetPath = 'DATA/RAW/REVIEWS/'
-filename = 'y_all_reviews.csv'
-content = 'result_contents.csv'
-
-fullPath = targetPath + filename
-datePath = 'CRAWLERS/'
-saveFullPath = datePath + content
 reviews = []
 def load_review_file() :
+    targetPath = 'DATA/RAW/REVIEWS/yeogi/'
+    filename = 'y_all_reviews.csv'
+
+    fullPath = targetPath + filename
     if os.path.exists(fullPath) :
         reviews = pd.read_csv(fullPath, encoding='utf-8-sig')
     else :
@@ -20,20 +16,17 @@ def load_review_file() :
         print("현재 작업 경로:", os.getcwd())
     return reviews
 
-#print(reviews.iloc[[2]].values)
-#old_date = reviews['write_date'].tolist()
 today = datetime.today()
 
-#date_df = pd.DataFrame(old_date)
-
+# 결측치 제거, 리뷰 텍스트 정제 
 def delete_none_data(reviews):
     content = reviews.dropna(subset=['review_content'], how='any', axis=0)
-    content = content.dropna(subset=['write_date'], how='any', axis=0)
-    print(content.info())
-
+    content = reviews.dropna(subset=['write_date'], how='any', axis=0)
+    content['name'] = (content['name'].str.replace(r"[^ㄱ-ㅎㅏ-ㅣ가-힣0-9 ]", "", regex=True).str.replace(r'\s+',' ',regex=True).str.strip())
+    content['review_content'] = (content['review_content'].str.replace(r"[^ㄱ-ㅎㅏ-ㅣ가-힣0-9 ]", "", regex=True).str.replace(r'\s+',' ',regex=True).str.strip())
     return content
 
-
+# 날짜 포멧
 def set_date(content) :
     today = datetime.today()
     converted_dates = []
@@ -63,45 +56,43 @@ def set_date(content) :
     print(content.info())
     return content
 
-
-# try :
-#     for review in reviews :
-#         review['content'] = str(re.sub(r"[^ㄱ-ㅎㅏ-ㅣ-가-힣0-9 ]", "", review['content'].get_text().strip()))
-        
-# except Exception as e :
-#     print(e)
-# print(df.iloc['id'])
 def main() :
+    # 파일 불러오기
     try :
         reviews = load_review_file()
         print("파일을 로드했습니다")
     except Exception as e :
         print(f"파일을 불러오는데 실패했습니다 {e}")
+        return
+    
+    # 결측치 제거
     try :
-        content = delete_none_data(reviews) 
+        new_content = delete_none_data(reviews)
+
     except Exception as e :
-        print(f"삭제하는 데 오류가 발생했습니다 {e}")
+        print(f"데이터를 정제하는 데 오류가 발생했습니다 {e}")
     try :
-        content['name'] = (content['name'].str.replace(r"[^ㄱ-ㅎㅏ-ㅣ가-힣0-9 ]", "", regex=True).str.replace(r'\s+',' ',regex=True).str.strip())
-        content['review_content'] = (content['review_content'].str.replace(r"[^ㄱ-ㅎㅏ-ㅣ가-힣0-9 ]", "", regex=True).str.replace(r'\s+',' ',regex=True).str.strip())
-        print("내용을 변경하였습니다")
-    except Exception as e :
-        print(f"내용을 변경하는데 실패했습니다 {e}")
-    try :
-        new_content = set_date(content)
+        content = set_date(new_content)
         print("날짜를 변경하였습니다")
     except Exception as e :
         print(f"날짜를 변경하는데 실패했습니다 {e}")
+    
+    # 리뷰 데이터
+    targetPath = 'DATA/RESULTS/'
+    saveFullPath = os.path.join(targetPath, 'y_cleaned_reviews_full.csv')
+    content.to_csv(saveFullPath, encoding='utf-8-sig', index=False, header=True)
 
-    new_df = pd.DataFrame(new_content)
-    new_df.to_csv(saveFullPath, encoding='utf-8-sig', index=False)
-    new_review_df = pd.DataFrame(content['review_content'].dropna())
-    filename = 'clean_reviews.csv'
-    savePath = datePath + filename
+    # 텍스트만 추출해서 저장 (빈 값 제외)
+    new_review_df = content['review_content'].dropna()
+    filename = 'y_cleaned_review_texts.csv'
+    savePath = os.path.join(targetPath, filename)
     new_review_df.to_csv(savePath, encoding='utf-8-sig', index=False, header=False)
+
+    # 중복 제거된 리뷰 저장
     duple_review = new_review_df.drop_duplicates()
-    filename = 'duple_reivews.csv'
-    savePath = datePath + filename
+    datePath = 'CRAWLERS/'
+    filename = 'y_reviews.csv'
+    savePath = os.path.join(datePath, filename)
     duple_review.to_csv(savePath, encoding='utf-8-sig', index=False, header=False)
 
 if __name__ == '__main__' :
